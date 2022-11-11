6e06e5f20fb91f0af376523b1b9332aee30c50ba
Gonzalo Coradello <gonzalocoradello@gmail.com>
2022-11-11 19:45:16 -0300

    added react-router-dom and cart context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/Navbar/Navbar';
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <Navbar />
-      </header>
-      <main>
-        <ItemListContainer />
-      </main>
-    </div>
+    <CartProvider>
+      <BrowserRouter>
+        <div className="App">
+          <header className="header">
+            <Navbar />
+          </header>
+          <main>
+              <Routes>
+                <Route path='/' element={<ItemListContainer />} />
+                <Route path='/category/:categoryId' element={<ItemListContainer />} />
+                {/* <Route path='/detail/:productId' element={<ItemDetailContainer />} />
+                <Route path='/cart' element={<CartContainer />} />
+                <Route path='/checkout' element={<CheckoutContainer />} /> */}
+                <Route path='*' element={ <h2>404 NOT FOUND</h2> } />
+              </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
