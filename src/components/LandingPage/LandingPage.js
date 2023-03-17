ecf98cf434ebc73bfaa8980ad3d357a77ea81e20
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-03-17 08:50:04 -0300

    refactor slides

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,7 +6,16 @@ import 'swiper/css';
 import 'swiper/css/autoplay';
 import { BsSearch } from 'react-icons/bs'
 import { Link } from "react-router-dom";
-import booksBackground from './assets/books.jpg'
+
+const books = [
+    {title: 'El Principito', src: '/images/saint-exupery_el-principito.png'},
+    {title: 'Romeo and Juliet', src: '/images/shakespeare_romeo-and-juliet.jpg'},
+    {title: 'Cumbres Borrascosas', src: '/images/bronte_cumbres-borrascosas.jpg'},
+    {title: 'Alicia en el País de las Maravillas', src: '/images/carroll_alicia-pais-maravillas.jpg'},
+    {title: 'Phantom of the Opera', src: '/images/leroux_phantom-of-the-opera.jpg'},
+    {title: 'El Cuervo', src: '/images/poe_el-cuervo.jpg'},
+    {title: 'Orgullo y Prejuicio', src: '/images/austen_pride-and-prejudice.jpg'}
+]
 
 const LandingPage = () => {
     return (
@@ -15,7 +24,7 @@ const LandingPage = () => {
                 <div className="bg-base-100 max-w-[1080px] mx-auto grid justify-center rounded-2xl relative z-10 shadow-md">
                     <div><Logo /></div>
                 </div>
-                <div className='shadow-md max-w-[900px] mx-auto mt-[-1rem] rounded-2xl'>
+                <div className='shadow-custom max-w-[900px] mx-auto mt-[-1rem] rounded-2xl'>
                     <div className="booksBackground py-10 rounded-2xl">
                         <div className='z-10 relative'>
                             <h2 className="font-heading text-3xl py-1">¿Qué libro quieres leer hoy?</h2>
@@ -31,7 +40,6 @@ const LandingPage = () => {
             <div className="container mx-auto px-4">
                 <h2 className="text-4xl font-heading text-secondary my-14">Inspírate y disfruta nuestras increíble historias</h2>
                 <h3 className="max-w-[900px] mx-auto text-3xl font-heading text-secondary text-left mb-8">Los más vendidos</h3>
-                {/* <Swiper className="carousel carousel-center max-w-[1000px] mx-auto p-4 space-x-20 bg-neutral rounded-box"> */}
                 <div className="">
                     <Swiper
                         className="max-w-[900px] bg-neutral"
@@ -62,28 +70,12 @@ const LandingPage = () => {
                                 loopedSlides: 3,
                             }
                         }}
-                    >
-                        <SwiperSlide>
-                            <img src="/images/saint-exupery_el-principito.png" className="rounded-box mx-auto max-h-64 md:max-h-72 lg:max-h-80" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/images/shakespeare_romeo-and-juliet.jpg" className="rounded-box mx-auto max-h-64 md:max-h-72 lg:max-h-80" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/images/bronte_cumbres-borrascosas.jpg" className="rounded-box mx-auto max-h-64 md:max-h-72 lg:max-h-80" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/images/carroll_alicia-pais-maravillas.jpg" className="rounded-box mx-auto max-h-64 md:max-h-72 lg:max-h-80" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/images/leroux_phantom-of-the-opera.jpg" className="rounded-box mx-auto max-h-64 md:max-h-72 lg:max-h-80" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/images/poe_el-cuervo.jpg" className="rounded-box mx-auto max-h-64 md:max-h-72 lg:max-h-80" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/images/austen_pride-and-prejudice.jpg" className="rounded-box mx-auto max-h-64 md:max-h-72 lg:max-h-80" />
-                        </SwiperSlide>
+                    >    
+                        { books.map(book => (
+                            <SwiperSlide key={book.title}>
+                                <img src={book.src} alt={book.title} className="rounded-box mx-auto max-h-64 md:max-h-72 lg:max-h-80" />
+                            </SwiperSlide>
+                        )) }
                     </Swiper>
                 </div>
                 <Link to="/books" className="btn btn-secondary btn-outline w-fit normal-case mt-8 grid ml-auto transition duration-500">Ver toda la colección</Link>
