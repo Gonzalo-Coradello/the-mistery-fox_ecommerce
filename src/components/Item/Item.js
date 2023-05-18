a96a4997642d03100ad49ef17c60f183bd336a03
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-05-18 19:05:34 -0300

    change thumbnails source

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,7 +4,7 @@ const Item = ({ _id, title, author, thumbnails, price }) => {
     return (
         <div className="card border border-primary-color ">
             <figure>
-                <div className="h-80"><img className="h-full" src={thumbnails[0]} alt={title} /></div>
+                <div className="h-80"><img className="h-full" src={`${process.env.REACT_APP_urlBase}${thumbnails[0]}`} alt={title} /></div>
             </figure>
             <div className="card-body">
                 <h2 className="card-title text-xl font-bold mx-auto">{title}</h2>
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -17,7 +17,7 @@ const ItemDetail = ({ _id, title, author, category, description, thumbnails, pri
     return (
         <div className="card lg:card-side gap-4 w-4/5 mx-auto mt-4">
             <div className="max-h-[500px] flex justify-center basis-1/2">
-                <img className="object-cover h-full rounded-xl mt-6" src={thumbnails[0]} alt={title} />
+                <img className="object-cover h-full rounded-xl mt-6" src={`${process.env.REACT_APP_urlBase}${thumbnails[0]}`} alt={title} />
             </div>
             <div className="card-body px-0 basis-1/2 lg:px-6">
                 <h2 className="card-title mx-auto text-xl font-heading">{title}</h2>
