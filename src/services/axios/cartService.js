ba11c9fabe94bb1d742c42aa5165e2960634904e
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-05-22 11:49:38 -0300

    add cart service

---
+++ src/services/axios/cartService.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { getCurrentUser } from "./userService";
+const url_base = `${process.env.REACT_APP_urlBase}/api/carts`
+
+export const getCartProducts = async () => {
+  const user = await getCurrentUser()
+  const response = await axios.get(`${url_base}/${user.payload.cart}`)
+  return response.data.payload.products
+}
+
+export const addProduct = async (pid, quantity) => {
+  const user = await getCurrentUser()
+  const response = await axios.post(`${url_base}/${user.payload.cart}/products/${pid}`, { quantity })
+  return response.data.payload.products
+}
+
+export const replaceQuantity = async (pid, quantity) => {
+  const user = await getCurrentUser()
+  const response = await axios.put(`${url_base}/${user.payload.cart}/products/${pid}`, { quantity })
+  return response.data.payload.products
+}
+
+export const deleteProduct = async (pid) => {
+  const user = await getCurrentUser()
+  const response = await axios.delete(`${url_base}/${user.payload.cart}/products/${pid}`)
+  return response.data
+}
+
+export const clearCart = async () => {
+  const user = await getCurrentUser()
+  const response = await axios.delete(`${url_base}/${user.payload.cart}`)
+  return response.data
+}
