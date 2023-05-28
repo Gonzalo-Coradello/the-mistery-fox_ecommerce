94e4370b78b4d3e373e3befefaca87ae366ee2f7
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-05-28 18:30:27 -0300

    register functionality on the frontend

--- src/components/sessions/Register/Register.js
+++ src/components/sessions/Register/Register.js
@@ -1,65 +1,105 @@
-import Button from "../../Buttons/Button";
-import GhostButton from "../../Buttons/GhostButton";
+import { useState } from 'react'
+import { useSessionContext } from '../../../context/UserContext'
+import { useNotification } from '../../../services/notification/NotificationService'
+import Button from '../../Buttons/Button'
+import { useNavigate } from 'react-router-dom'
+import GhostLink from '../../Buttons/GhostLink'
 
 const Register = () => {
+  const { register } = useSessionContext()
+  const { setNotification } = useNotification()
+  const navigate = useNavigate()
+
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    age: '',
+    password: '',
+  })
+
+  const handleChange = e => {
+    setFormData(prev => ({ ...prev, [e.target.name]: e.target.value }))
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const response = await register(formData)
+    if(response.status !== 'success') {
+      setNotification('error', 'Ha ocurrido un error')
+      return
+    }
+    setNotification('success', '¡Registro exitoso!')
+    navigate('/sessions/login')
+  }
+
   return (
     <section>
-      <h2 class="text-2xl mt-12 mb-8">Registrar usuarios</h2>
-      <form action="/sessions/register" method="post">
+      <h2 className='text-2xl mt-12 mb-8'>Registrar usuarios</h2>
+
+      <form>
         <input
           required
-          type="text"
-          name="first_name"
-          placeholder="First name"
-          class="input input-bordered border-white w-full max-w-xs mb-2"
+          type='text'
+          name='first_name'
+          placeholder='Nombre'
+          value={formData.first_name}
+          onChange={handleChange}
+          className='input input-bordered border-white w-full max-w-xs mb-2'
         />
         <br></br>
         <input
           required
-          type="text"
-          name="last_name"
-          placeholder="Last name"
-          class="input input-bordered border-white w-full max-w-xs mb-2"
+          type='text'
+          name='last_name'
+          placeholder='Last name'
+          value={formData.last_name}
+          onChange={handleChange}
+          className='input input-bordered border-white w-full max-w-xs mb-2'
         />
         <br></br>
         <input
           required
-          type="email"
-          name="email"
-          placeholder="Email"
-          class="input input-bordered border-white w-full max-w-xs mb-2"
+          type='email'
+          name='email'
+          placeholder='Email'
+          value={formData.email}
+          onChange={handleChange}
+          className='input input-bordered border-white w-full max-w-xs mb-2'
         />
         <br></br>
         <input
           required
-          type="tel"
-          name="age"
-          placeholder="Age"
-          class="input input-bordered border-white w-full max-w-xs mb-2"
+          type='tel'
+          name='age'
+          placeholder='Age'
+          value={formData.age}
+          onChange={handleChange}
+          className='input input-bordered border-white w-full max-w-xs mb-2'
         />
         <br></br>
         <input
           required
-          type="password"
-          name="password"
-          placeholder="Password"
-          class="input input-bordered border-white w-full max-w-xs mb-2"
+          type='password'
+          name='password'
+          placeholder='Password'
+          value={formData.password}
+          onChange={handleChange}
+          className='input input-bordered border-white w-full max-w-xs mb-2'
         />
         <br></br>
 
-        <Button>
-          Enviar
-        </Button>
+        <Button handleClick={handleSubmit}>Enviar</Button>
       </form>
 
-      <p className="mt-8">¿Ya estás registrado?</p>
+      <p className='mt-8'>¿Ya estás registrado?</p>
       <p>
-        <GhostButton url="/sessions/login" classes='font-semibold my-2'>
+        <GhostLink url='/sessions/login' classes='font-semibold my-2'>
           Inicia sesión
-        </GhostButton>
+        </GhostLink>
       </p>
     </section>
-  );
-};
+  )
+}
 
 export default Register
