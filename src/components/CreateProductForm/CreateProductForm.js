ce1776fd2368e2e9ddc9d916cff1d57f5061629c
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-05-30 02:45:19 -0300

    add create product form

--- src/components/CreateProductForm/CreateProductForm.js
+++ src/components/CreateProductForm/CreateProductForm.js
@@ -1,76 +1,206 @@
+import { useState } from "react"
+import Select from 'react-select'
+import Button from "../Buttons/Button"
+import Product from "../../services/axios/productService"
+import { useNotification } from '../../services/notification/NotificationService'
+import { useNavigate } from "react-router-dom"
+const productService = new Product()
+
 const CreateProductForm = () => {
+
+  const { setNotification } = useNotification()
+  const navigate = useNavigate()
+
+  const [formData, setFormData] = useState({
+    event: 'product',
+    title: '',
+    author: '',
+    description: '',
+    lang: '',
+    code: '',
+    price: '',
+    stock: '',
+    categories: [],
+    file: '',
+  })
+
+  const options = [
+    { value: 'romance', label: 'Romance' },
+    { value: 'fantasia', label: 'Fantasía' },
+    { value: 'terror', label: 'Terror' },
+    { value: 'teatral', label: 'Teatral' },
+    { value: 'ciencia-ficcion', label:  'Ciencia Ficción' },
+    { value: 'policial', label: 'Policial' },
+    { value: 'aventura', label: 'Aventura' },
+    { value: 'juvenil', label: 'Juvenil' },
+    { value: 'coleccion', label: 'Colecciones' },
+    { value: 'novela', label: 'Novelas' },
+    { value: 'cuento', label: 'Cuentos y relatos' },
+  ]
+
+  const handleChange = e => {
+    setFormData(prev => ({ ...prev, [e.target.name]: e.target.value }))
+  }
+
+  const handleSelect = e => {
+    const categories = e.map(obj => obj.value)
+    setFormData(prev => ({ ...prev, categories }))
+  }
+
+  const handleFile = e => {
+    setFormData(prev => ({ ...prev, file: e.target.files[0] }))
+  }
+ 
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const product = await productService.createProduct(formData)
+    if(product) {
+      setNotification('success', '¡Producto creado correctamente!')
+      navigate(`/detail/${product._id}`)
+    }
+  }
+   
   return (
-    <div>
-      <h2 className='text-3xl mb-8'>Crear producto</h2>
+    <div className='mt-8'>
+      <h2 className='text-3xl'>Crear producto</h2>
       <div className='px-4 max-w-[80vw] mx-auto'>
-        <form action='/products' method='post' enctype='multipart/form-data'>
-          <div className='grid lg:grid-cols-4 gap-4'>
-            <input type='text' name='event' value='product' hidden></input>
+        <form>
+          <div className='grid lg:grid-cols-3 gap-4 justify-center my-8'>
             <div>
-              <h4>Título: </h4>
               <input
+                required
                 type='text'
                 name='title'
-                className='input input-bordered w-full max-w-xs'></input>
+                placeholder='Título'
+                value={formData.title}
+                onChange={handleChange}
+                className='input input-bordered w-full max-w-xs border-primary'></input>
             </div>
             <div>
-              <h4>Autor: </h4>
               <input
+                required
                 type='text'
                 name='author'
-                className='input input-bordered w-full max-w-xs'></input>
+                placeholder='Autor'
+                value={formData.author}
+                onChange={handleChange}
+                className='input input-bordered w-full max-w-xs border-primary'></input>
             </div>
             <div>
-              <h4>Descripción: </h4>
               <input
+                required
                 type='text'
                 name='description'
-                className='input input-bordered w-full max-w-xs'></input>
+                placeholder='Descripción'
+                value={formData.description}
+                onChange={handleChange}
+                className='input input-bordered w-full max-w-xs border-primary'></input>
             </div>
             <div>
-              <h4>Idioma: </h4>
               <input
+                required
                 type='text'
                 name='lang'
-                className='input input-bordered w-full max-w-xs'></input>
+                placeholder='Idioma'
+                value={formData.lang}
+                onChange={handleChange}
+                className='input input-bordered w-full max-w-xs border-primary'></input>
             </div>
             <div>
-              <h4>Código: </h4>
               <input
+                required
                 type='text'
                 name='code'
-                className='input input-bordered w-full max-w-xs'></input>
+                placeholder='Código'
+                value={formData.code}
+                onChange={handleChange}
+                className='input input-bordered w-full max-w-xs border-primary'></input>
             </div>
             <div>
-              <h4>Precio: </h4>
               <input
+                required
                 type='number'
                 name='price'
-                className='input input-bordered w-full max-w-xs'></input>
+                placeholder='Precio'
+                value={formData.price}
+                onChange={handleChange}
+                className='input input-bordered w-full max-w-xs border-primary'></input>
             </div>
             <div>
-              <h4>Stock: </h4>
               <input
+                required
                 type='number'
                 name='stock'
-                className='input input-bordered w-full max-w-xs'></input>
+                placeholder='Stock'
+                value={formData.stock}
+                onChange={handleChange}
+                className='input input-bordered w-full max-w-xs border-primary'></input>
             </div>
             <div>
-              <h4>Categorías: </h4>
-              <input
-                type='text'
-                name='categories'
-                className='input input-bordered w-full max-w-xs'></input>
+                <Select 
+                  options={options}
+                  isMulti={true}
+                  required={true}
+                  name='categories'
+                  placeholder='Categorías'
+                  onChange={handleSelect}
+                  classNames={{
+                    container: () => 'input-bordered w-full max-w-xs mx-auto min-h-12 grid',
+                    multiValueRemove: () => 'btn btn-ghost btn-xs btn-circle m-[2px]'
+                  }}
+                  styles={{
+                    control: (baseStyles, state) => ({
+                      ...baseStyles,
+                      backgroundColor: 'transparent',
+                      border: 'none',
+                      textAlign: 'left',
+                      // paddingInline: '0.25rem',
+                      boxShadow: 'none'
+                    }),
+                    container: (baseStyles) => ({
+                      ...baseStyles,
+                      border: '1px solid #FFF',
+                      borderRadius: '0.5rem'
+                    }),
+                    menu: (baseStyles, state) => ({
+                      ...baseStyles,
+                      backgroundColor: '#1b2213',
+                    }),
+                    option: (baseStyles, state) => ({
+                      ...baseStyles,
+                      backgroundColor: state.isFocused ? '#243016' : '#1b2213'
+                    }),
+                    placeholder: (baseStyles, state) => ({
+                      ...baseStyles,
+                      color: '#9aa1ac',
+                      paddingLeft: '0.25rem'
+                    }),
+                    multiValue: (baseStyles) => ({
+                      ...baseStyles,
+                      backgroundColor: 'transparent',
+                      border: '1px solid #FFF',
+                      borderRadius: '1rem',
+                      padding: '3px'
+                    }),
+                    multiValueLabel: (baseStyles) => ({
+                      ...baseStyles,
+                      color: '#FFF',
+                    }),
+                    multiValueRemove: (baseStyles, state) => ({}),
+                  }} 
+                />
             </div>
             <div>
-              <h4>Imágenes: </h4>
               <input
+                required
                 type='file'
                 name='file'
-                className='input input-bordered w-full max-w-xs'></input>
+                placeholder='Imágenes'
+                onChange={handleFile}
+                className='input input-bordered w-full max-w-xs border-primary pt-[0.7rem]'></input>
             </div>
           </div>
-          <input type='submit' value='Crear' className='btn'></input>
+          <Button handleClick={handleSubmit}>Crear</Button>
         </form>
       </div>
     </div>
