7213d703d161c540a3e9df8be02ef9304ca8e480
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-05-30 21:44:21 -0300

    add functions to user service

--- src/services/axios/userService.js
+++ src/services/axios/userService.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 const url_base = `${process.env.REACT_APP_urlBase}/api/sessions`
+const url_base_users = `${process.env.REACT_APP_urlBase}/api/users`
 axios.defaults.withCredentials = true
 
 export default class User {
@@ -33,6 +34,47 @@ export default class User {
   logout = async () => {
     try {
       const response = await axios.post(`${url_base}/logout`)
+      if (!response) return { status: 'error', error: 'Auth error' }
+      return response?.data
+    } catch (error) {
+      return { status: 'error', error }
+    }
+  }
+
+  getAllUsers = async () => {
+    try {
+      const response = await axios.get(`${url_base_users}`)
+      console.log(response)
+      if (!response) return { status: 'error', error: 'Auth error' }
+      return response?.data.payload
+    } catch (error) {
+      return { status: 'error', error }
+    }
+  }
+
+  deleteInactiveUsers = async () => {
+    try {
+      const response = await axios.delete(`${url_base_users}`)
+      if (!response) return { status: 'error', error: 'Auth error' }
+      return response?.data
+    } catch (error) {
+      return { status: 'error', error }
+    }
+  }
+
+  updateRole = async id => {
+    try {
+      const response = await axios.put(`${url_base_users}/premium/${id}`)
+      if (!response) return { status: 'error', error: 'Auth error' }
+      return response?.data
+    } catch (error) {
+      return { status: 'error', error }
+    }
+  }
+
+  uploadDocuments = async (id, data) => {
+    try {
+      const response = await axios.post(`${url_base_users}/${id}/documents`, data)
       if (!response) return { status: 'error', error: 'Auth error' }
       return response?.data
     } catch (error) {
