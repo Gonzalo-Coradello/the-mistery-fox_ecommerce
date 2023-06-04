d5c427727129ead8ba8d22fe949aa54c4c38f449
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-06-04 00:56:33 -0300

    improve styling

--- src/components/Buttons/LinkButton.js
+++ src/components/Buttons/LinkButton.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const LinkButton = ({ url, children, classes }) => {
   return (
-    <Link to={url} className={ `btn btn-outline mt-4 normal-case w-32 transition duration-500 ${classes}` }>
+    <Link to={url} className={ `btn btn-outline mt-4 normal-case w-32 min-w-max transition duration-500 ${classes}` }>
       {children}
     </Link>
   )
