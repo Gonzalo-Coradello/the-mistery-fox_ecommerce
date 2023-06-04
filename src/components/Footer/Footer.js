6c48f454b34d47325879cdd9f4694268610078fb
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-06-04 18:42:32 -0300

    create footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,51 +1,72 @@
+import { useEffect, useState } from 'react'
+import { useSessionContext } from '../../context/UserContext'
+import GhostLink from '../Buttons/GhostLink'
+import { FaGithub, FaLinkedinIn, FaInstagram, FaBehance, FaLaptopCode } from 'react-icons/fa'
+import GhostButton from '../Buttons/GhostButton'
+
+const socialLinks = [
+  { key: 1, name: 'Portfolio Gonzalo', icon: <FaLaptopCode after='Portfolio Gonzalo' size={30} />, url: 'https://gonzalodev.vercel.app/' },
+  { key: 2, name: 'LinkedIn Gonzalo', icon: <FaLinkedinIn after='LinkedIn Gonzalo' size={30} />, url: 'https://www.linkedin.com/in/gonzalo-coradello/' },
+  { key: 3, name: 'Github Gonzalo', icon: <FaGithub after='Github Gonzalo' size={30} />, url: 'https://github.com/Gonzalo-Coradello' },
+  { key: 4, name: 'Behance Talia', icon: <FaBehance after='' size={30} />, url: 'https://www.behance.net/talialaborde' },
+  { key: 5, name: 'LinkedIn Talia', icon: <FaLinkedinIn after='LinkedIn Talia' size={30} />, url: 'https://www.linkedin.com/in/talia-laborde/' },
+  { key: 6, name: 'Instagram Linné Studio', icon: <FaInstagram after='Instagram Linné Studio' size={30} />, url: 'https://www.instagram.com/linnestudio_/' },
+]
+
+
 const Footer = () => {
+
+  const { user, isLogged } = useSessionContext()
+  const [links, setLinks] = useState([])
+
+  useEffect(() => {
+    const currentLinks = [
+      { key: 1, path: '/', slug: 'inicio' },
+      { key: 2, path: '/books', slug: 'nuestros libros' },
+      { key: 3, path: 'mailto:gonzalocoradello@gmail.com', slug: 'contacto' },
+    ]
+
+    isLogged
+      ? user.role === 'admin'
+        ? currentLinks.push(
+            // ADMIN ↓
+            { key: 4, path: '/products', slug: 'administrar productos' },
+            { key: 5, path: '/users', slug: 'administrar usuarios' },
+            { key: 6, path: '/sessions/user', slug: 'mi cuenta' }
+          )
+        : user.role === 'premium'
+        ? currentLinks.push(
+            // PREMIUM ↓
+            { key: 4, path: '/products', slug: 'administrar productos' },
+            { key: 6, path: '/sessions/user', slug: 'mi cuenta' }
+          )
+        : currentLinks.push({
+            // USER ↓
+            key: 6,
+            path: '/sessions/user',
+            slug: 'mi cuenta',
+          })
+      : currentLinks.push(
+          // NOT LOGGED IN ↓
+          { key: 7, path: '/sessions/login', slug: 'iniciar sesión' },
+          { key: 8, path: '/sessions/register', slug: 'registrarse' }
+        )
+
+    setLinks(currentLinks)
+  }, [user, isLogged])
+
   return (
     <footer className='footer footer-center px-10 py-8 bg-base-300 text-base-content rounded'>
       <div className='grid grid-flow-col gap-4'>
-        <a className='link link-hover'>About us</a>
-        <a className='link link-hover'>Contact</a>
-        <a className='link link-hover'>Jobs</a>
-        <a className='link link-hover'>Press kit</a>
+        { links.map(({ key, path, slug }) => slug === 'contacto' ? <GhostButton><a href={path}>{slug}</a></GhostButton> : <GhostLink key={key} url={path} classes='min-w-max'>{slug}</GhostLink>)}
       </div>
       <div>
-        <div className='grid grid-flow-col gap-4'>
-          <a>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              className='fill-current'
-            >
-              <path d='M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z'></path>
-            </svg>
-          </a>
-          <a>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              className='fill-current'
-            >
-              <path d='M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z'></path>
-            </svg>
-          </a>
-          <a>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              className='fill-current'
-            >
-              <path d='M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z'></path>
-            </svg>
-          </a>
+        <div className='grid grid-flow-col gap-8'>
+          { socialLinks.map(({ key, name, icon, url }) => <GhostButton key={key} square={true} classes='btn-md' ><a href={url} target='_blank' rel='noreferrer' >{icon}</a></GhostButton>) }
         </div>
       </div>
       <div>
-        <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+        <p>Copyright © 2023 - Todos los derechos reservados</p>
       </div>
     </footer>
   )
