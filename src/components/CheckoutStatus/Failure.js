84b85940e86fbfc022db88b50ebe1283a171b353
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-06-05 22:30:03 -0300

    add styles

--- src/components/CheckoutStatus/Failure.js
+++ src/components/CheckoutStatus/Failure.js
@@ -1,7 +1,13 @@
+import LinkButton from '../Buttons/LinkButton'
+
 const Failure = () => {
   return (
     <section>
-      <h2>Error en el pago</h2>
+      <div className='border border-primary-color p-12 grid gap-4 mx-4 my-16 max-w-[700px] lg:mx-auto'>
+        <h2 className='text-2xl font-semibold mb-4'>Error</h2>
+        <p>Hubo un error en el pago</p>
+        <LinkButton url='/cart'>Volver al carrito</LinkButton>
+      </div>
     </section>
   )
 }
